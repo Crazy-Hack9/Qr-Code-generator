9ea602ecbf606e7a0c0f106febef4db7cbcdb1c7
Hack9_MrNoX <86767023+Hack9-MrNoX@users.noreply.github.com>
2022-05-22 00:32:51 +0530

    Update QR_Code.py

--- QR_Code.py
+++ QR_Code.py
@@ -3,7 +3,7 @@
 
 from cgitb import text
 import qrcode
-Data = input("\033[1;32;40mEnter the Link & Text for QR \t \n -=>")
-print("\033[1;32;40m Bright Green  \n")
+Data = input("Enter the Link & Text for QR \t \n -=>")
+
 img = qrcode.make(Data)
 img.save("QR.jpg")
